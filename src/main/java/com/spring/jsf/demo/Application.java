c1361e2c655f89a36f5e1fe127fb0894f9965bf3
Emre Burak ERKEN <emre.erken@esb.org.tr>
2019-08-27 14:34:47 +0300

    Spring-Boot with JSF - static page

---
+++ src/main/java/com/spring/jsf/demo/Application.java
@@ -0,0 +1,24 @@
+package com.spring.jsf.demo;
+
+import javax.faces.webapp.FacesServlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean<FacesServlet> servletRegistrationBean() {
+		FacesServlet servlet = new FacesServlet();
+		ServletRegistrationBean<FacesServlet> servletRegistrationBean = new ServletRegistrationBean<FacesServlet>(servlet, "*.jsf");
+		return servletRegistrationBean;
+	}
+}
